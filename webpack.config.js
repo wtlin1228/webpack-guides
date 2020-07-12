f1243c4ab446b6bdda50eae3eb08c5acee939640
wtlin1228 <wtlin1228@gmail.com>
2020-07-12 15:39:12 +0800

    split chunks plugin

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: "development",
   entry: {
     app: "./src/index.js",
-    print: "./src/print.js",
+    another: "./src/another-module.js",
   },
   devtool: "inline-source-map",
   devServer: {
@@ -20,8 +20,13 @@ module.exports = {
   ],
   output: {
     filename: "[name].bundle.js",
+    chunkFilename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
   },
   module: {
     rules: [
